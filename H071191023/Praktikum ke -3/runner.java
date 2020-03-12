0d5ae1f35e9f9f191be479d613ffe6d3bfdd1e09
indiraseptianita <indiraseptianita269@gmail.com>
2020-03-12 22:08:19 +0800

    added praktikum 3

---
+++ H071191023/Praktikum ke -3/runner.java
@@ -0,0 +1,105 @@
+import java.util.*;
+public class runner {
+
+    public static void main(String[] args) {
+        Scanner nd = new Scanner(System.in);
+        System.out.println("welcome to the battle simulator");
+        System.out.println("insert player 1");
+        System.out.println("name");
+        String name = nd.next();
+        System.out.println("role");
+        String role = nd.next();
+        Player player1 = new Player(name, role);
+        System.out.println();
+        System.out.println("insert player 2");
+        System.out.println("name");
+        String name2 = nd.next();
+        System.out.println("role");
+        String role2 = nd.next();
+        Player player2 = new Player(name2, role2);
+
+        int p1 = 0;
+        int p2 = 0;
+        while(true){
+            if(player1.dead(player2) || player2.dead(player1)){
+                if(player1.dead(player2)){
+                    System.out.println(player1.getName() + "dead");
+                    System.out.println();
+                    System.out.println(player2.getName() + "win");
+                }else{
+                    System.out.println(player2.getName() + "dead");
+                    System.out.println();
+                    System.out.println(player1.getName() + "win");
+                }
+            }
+
+            //player1
+            System.out.println();
+            System.out.println(player1.getName() + " turn's ");
+            System.out.println("1. show status");
+            System.out.println("2. show items");
+            System.out.println("3. attack");
+            System.out.println("4. heal");
+            System.out.println("5. buy items");
+            System.out.println("6. sell items");
+            System.out.println("7. quit");
+            p1 = nd.nextInt();
+            if(p1 == 1){
+                player1.showStatus();
+            
+            }else if(p1 == 2){
+                player1.showItems();
+            }else if(p1 == 3){
+              //  player2.getDamage();
+              System.out.println(player2.getDamage(player1));
+            }else if(p1 == 4){
+                player1.healing(player1);
+            }else if(p1 == 5){
+                System.out.println("beli Hp potion");
+                int item = nd.nextInt();
+                player1.buy(player1, item);
+            }else if(p1 == 6){
+                System.out.println("item ke? ");
+                int jual = nd.nextInt();
+                player1.sell(jual);
+            }else if(p1 == 7){
+                return;
+            }else if(player1 == player2){
+            System.out.println(player2.getAttack());
+            }
+
+            //player2
+            System.out.println();
+            System.out.println(player2.getName() + " turn's ");
+            System.out.println("1. show status");
+            System.out.println("2. show items");
+            System.out.println("3. attack");
+            System.out.println("4. heal");
+            System.out.println("5. buy items");
+            System.out.println("6. sell items");
+            System.out.println("7. quit");
+            System.out.println();
+            p2 = nd.nextInt();
+            if(p2 == 1){
+                player2.showStatus();
+            }else if(p2 == 2){
+                player2.showItems();
+            }else if(p2 == 3){
+               // player1.getDamage(player1);
+                System.out.println(player1.getDamage(player2));
+            }else if(p2 == 4){
+                player1.healing(player2);
+            }else if(p2 == 5){
+                System.out.println("beli Hp potion");
+                int item2 = nd.nextInt();
+                player2.buy(player2, item2);
+            }else if(p2 == 6){
+                System.out.println("item ke? ");
+                int jual = nd.nextInt();
+                player1.sell(jual);
+            }else if(p2 == 7){
+                return;
+            }
+        }
+    }
+}
